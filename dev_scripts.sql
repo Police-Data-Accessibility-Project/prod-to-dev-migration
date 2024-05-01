aa0528ed647017624b70b04f5a4abd6cf4f21a54
Max Chis <48846180+maxachis@users.noreply.github.com>
2024-05-01 16:49:56 -0400

    Update dev_scripts.sql

--- dev_scripts.sql
+++ dev_scripts.sql
@@ -8,7 +8,5 @@ CREATE TABLE IF NOT EXISTS public.migration_test
     test_id integer NOT NULL DEFAULT nextval('test_test_id_seq'::regclass)
 )
 
-TABLESPACE pg_default;
-
 ALTER TABLE IF EXISTS public.migration_test
     OWNER to doadmin;
