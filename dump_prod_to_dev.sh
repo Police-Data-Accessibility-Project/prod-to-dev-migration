7a28b7e124f0153c9e57886fa02799637d75e7ad
Max Chis <48846180+maxachis@users.noreply.github.com>
2024-05-01 16:58:33 -0400

    Update dump_prod_to_dev.sh

--- dump_prod_to_dev.sh
+++ dump_prod_to_dev.sh
@@ -15,7 +15,7 @@ echo "Dumping production database..."
 pg_dump $PROD_DB_CONN_STRING > $DUMP_FILE
 
 echo "Dropping all connections to the development database..."
-psql -d $DEV_ADMIN_DB_CONN_STRING -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'dev_dbname' AND pid <> pg_backend_pid();"
+psql -d $DEV_ADMIN_DB_CONN_STRING -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'pdap_dev_db' AND pid <> pg_backend_pid();"
 
 echo "Dropping the development database..."
 psql -d $DEV_ADMIN_DB_CONN_STRING -c "DROP DATABASE IF EXISTS pdap_dev_db;"
