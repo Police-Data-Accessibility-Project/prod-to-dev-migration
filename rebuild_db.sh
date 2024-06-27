d33b5dfedf98d7cac503fdd530feadfe84d8e7c1
maxachis <maxachis@gmail.com>
2024-06-27 11:36:46 -0400

    Updated rebuild_db.sh script to handle database connections more efficiently, using a dynamic target database variable.

--- rebuild_db.sh
+++ rebuild_db.sh
@@ -5,17 +5,28 @@
 ADMIN_DB_CONN_STRING=$1
 DUMP_FILE=$2
 
+TARGET_DB=pdap_dev_db
+
 # Change directory to the location of the script
 cd "$(dirname "$0")"
 
-echo "Dropping all connections to the database..."
-psql -d $ADMIN_DB_CONN_STRING -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'pdap_dev_db' AND pid <> pg_backend_pid();"
+echo "Dropping all connections to the $TARGET_DB database..."
+psql -d $ADMIN_DB_CONN_STRING -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$TARGET_DB' AND pid <> pg_backend_pid();"
+
+CONNECTIONS=$(psql -d $ADMIN_DB_CONN_STRING -c "SELECT COUNT(*) FROM pg_stat_activity WHERE datname = '$TARGET_DB';")
+
+# Verify if there are no active connections
+if [ "$CONNECTIONS" -eq 0 ]; then
+  echo "All connections to the database '$TARGET_DB' have been successfully terminated."
+else
+  echo "There are still active connections to the database '$TARGET_DB'."
+fi
 
 echo "Dropping the database..."
-psql -d $ADMIN_DB_CONN_STRING -c "DROP DATABASE IF EXISTS pdap_dev_db;"
+psql -d $ADMIN_DB_CONN_STRING -c "DROP DATABASE IF EXISTS $TARGET_DB;"
 
 echo "Creating database..."
-psql -d $ADMIN_DB_CONN_STRING -c "CREATE DATABASE pdap_dev_db;"
+psql -d $ADMIN_DB_CONN_STRING -c "CREATE DATABASE $TARGET_DB;"
 
 echo "Restoring dump to database..."
 psql $ADMIN_DB_CONN_STRING < $DUMP_FILE
