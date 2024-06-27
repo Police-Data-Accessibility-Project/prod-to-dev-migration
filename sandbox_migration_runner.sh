d87b38eca9428a6024beac7d057b276d3e2267c3
maxachis <maxachis@gmail.com>
2024-06-27 17:01:23 -0400

    Added migration scripts for sandbox and staging environments
    Updated sandbox_migration_runner.sh and stg_migration_runner.sh scripts to include commands for dumping and rebuilding databases from production, as well as creating database users. Also added shebang and environment variable loading.

---
+++ sandbox_migration_runner.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Change directory to the location of the script
+cd "$(dirname "$0")"
+
+# Load environment variables
+if [ -f .env ]; then
+  export $(grep -v '^#' .env | xargs)
+fi
+
+./setup.sh
+
+# Dump and rebuild the sandbox database from production
+./dump_prod.sh $PROD_DB_CONN_STRING 'true'
+./rebuild_db.sh $SANDBOX_ADMIN_DB_CONN_STRING $SANDBOX_TARGET_DB_CONN_STRING 'prod_schema.dump' pdap_test_db
+./create_db_user.sh $SANDBOX_ADMIN_DB_CONN_STRING $SANDBOX_DB_USER $SANDBOX_DB_PASSWORD
---
+++ stg_migration_runner.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Change directory to the location of the script
+cd "$(dirname "$0")"
+
+# Load environment variables
+if [ -f .env ]; then
+  export $(grep -v '^#' .env | xargs)
+fi
+
+./setup.sh
+
+# Dump and rebuild the stage database from production
+./dump_prod.sh $PROD_DB_CONN_STRING
+./rebuild_db.sh $STG_ADMIN_DB_CONN_STRING $STG_TARGET_DB_CONN_STRING 'prod.dump' pdap_dev_db
+./create_db_user.sh $STG_ADMIN_DB_CONN_STRING $STG_DB_USER $STG_DB_PASSWORD
