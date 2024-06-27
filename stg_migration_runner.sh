c3abd5798f476356f05bfc365ee49362a213eeed
maxachis <maxachis@gmail.com>
2024-06-27 19:39:06 -0400

    Refactored stg_migration_runner.sh to use variable for target database name instead of hardcoding it.

--- stg_migration_runner.sh
+++ stg_migration_runner.sh
@@ -12,5 +12,5 @@ fi
 
 # Dump and rebuild the stage database from production
 ./dump_prod.sh $PROD_DB_CONN_STRING
-./rebuild_db.sh $STG_ADMIN_DB_CONN_STRING $STG_TARGET_DB_CONN_STRING 'prod.dump' pdap_dev_db
+./rebuild_db.sh $STG_ADMIN_DB_CONN_STRING $STG_TARGET_DB_CONN_STRING 'prod.dump' $STG_TARGET_DB
 ./create_db_user.sh $STG_ADMIN_DB_CONN_STRING $STG_DB_USER $STG_DB_PASSWORD
