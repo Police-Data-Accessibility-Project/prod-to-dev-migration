3ac0995692fe21f587080416b4c764f0d022dd4d
maxachis <maxachis@gmail.com>
2024-06-28 18:32:18 -0400

    Add python logic

--- setup.sh
+++ setup.sh
@@ -5,3 +5,8 @@ cd "$(dirname "$0")"
 ./apt.postgresql.org.sh -y
 apt-get update
 apt-get install postgresql-15 postgresql-client-15 -y
+add-apt-repository ppa:deadsnakes/ppa
+apt-get install python3.11 -y
+python3 -m venv migration_venv
+source migration_venv/bin/activate
+pip install -r requirements.txt
