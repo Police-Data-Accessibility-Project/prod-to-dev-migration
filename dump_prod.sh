87f4dbef9464f9bfaad0c21e5e0c230da8bf2971
maxachis <maxachis@gmail.com>
2024-06-27 21:01:23 -0400

    Updated dump_prod.sh to include data from specific tables in addition to the schema dump.

--- dump_prod.sh
+++ dump_prod.sh
@@ -2,18 +2,20 @@
 # This script will dump the production database to a .sql file in the current
 # directory, with options to either dump the entire database or just the schema.
 PROD_DB_CONN_STRING=$1
-SCHEMA_ONLY=$2
+SANDBOX_DUMP=$2
 
 # Change directory to the location of the script
 cd "$(dirname "$0")"
 
-# If SCHEMA_ONLY is not set, then dump the entire database.
-if [[ -z "$SCHEMA_ONLY" ]]; then
+# If SANDBOX_DUMP is not set, then dump the entire database.
+if [[ -z "$SANDBOX_DUMP" ]]; then
   echo "Dumping production database..."
   DUMP_FILE="prod.dump"
   pg_dump "$PROD_DB_CONN_STRING" -Fc --no-owner --no-acl > $DUMP_FILE
 else
-  echo "Dumping production schema..."
+  echo "Dumping production data for sandbox..."
   DUMP_FILE="prod_schema.dump"
-  pg_dump "$PROD_DB_CONN_STRING" -Fc --schema-only --no-owner --no-acl > $DUMP_FILE
+  pg_dump "$PROD_DB_CONN_STRING" -Fp --schema-only --no-owner --no-acl > $DUMP_FILE
+  # Additionally, dump data from most but not all tables
+  pg_dump "$PROD_DB_CONN_STRING" -Fp --data-only --no-owner --no-acl -T quick_search_query_logs -T data_requests -T users -T access_tokens -T session_tokens >> $DUMP_FILE
 fi
