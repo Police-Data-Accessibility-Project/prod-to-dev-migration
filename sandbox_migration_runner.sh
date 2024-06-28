2ea7169b120860ea6f008ecdd6888de550ae3537
maxachis <maxachis@gmail.com>
2024-06-27 21:12:36 -0400

    Updated create_db_user.sh script to grant CRUD privileges to the dev user on the target database instead of the dev database.

--- sandbox_migration_runner.sh
+++ sandbox_migration_runner.sh
@@ -13,4 +13,4 @@ fi
 # Dump and rebuild the sandbox database from production
 ./dump_prod.sh $PROD_DB_CONN_STRING 'true'
 ./rebuild_db.sh $SANDBOX_ADMIN_DB_CONN_STRING $SANDBOX_TARGET_DB_CONN_STRING 'prod_to_sandbox.sql' $SANDBOX_TARGET_DB
-./create_db_user.sh $SANDBOX_ADMIN_DB_CONN_STRING $SANDBOX_DB_USER $SANDBOX_DB_PASSWORD
+./create_db_user.sh $SANDBOX_ADMIN_DB_CONN_STRING $SANDBOX_DB_USER $SANDBOX_DB_PASSWORD $SANDBOX_TARGET_DB
