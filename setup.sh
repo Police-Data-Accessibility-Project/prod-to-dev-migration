76b432b3a98aa404313919acd8c4e7cb93fe3286
maxachis <maxachis@gmail.com>
2024-06-29 17:51:20 -0400

    Attempt to correct pip command not found error

--- setup.sh
+++ setup.sh
@@ -5,7 +5,15 @@
 apt-get update
 apt-get install postgresql-15 postgresql-client-15 -y
 apt-get install software-properties-common -y
-add-apt-repository ppa:deadsnakes/ppa -y
-apt-get install python3.11 -y
-python3 -m ensurepip
+apt-get curl -y
+curl https://pyenv.run | bash
+
+# Add pyenv to PATH
+PATH="/root/.pyenv/bin:$PATH"
+echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+
+#add-apt-repository ppa:deadsnakes/ppa -y
+#apt-get install python3.11 -y
+#python3 -m ensurepip
 pip install -r requirements.txt
