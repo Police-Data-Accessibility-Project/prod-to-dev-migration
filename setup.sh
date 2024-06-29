16c787b754abb86edfcdd95d9829a2057b9ff44e
maxachis <maxachis@gmail.com>
2024-06-29 17:42:05 -0400

    Attempt to correct pip command not found error

--- setup.sh
+++ setup.sh
@@ -7,4 +7,5 @@ apt-get install postgresql-15 postgresql-client-15 -y
 apt-get install software-properties-common -y
 add-apt-repository ppa:deadsnakes/ppa -y
 apt-get install python3.11 -y
-pip3 install -r requirements.txt
+python3 -m ensurepip
+pip install -r requirements.txt
