66e56c01cf507a2af30c2bfad493a6945274c503
maxachis <maxachis@gmail.com>
2024-07-06 18:00:13 -0400

    Reorder Jenkinsfile stages, add chmod to stage_migration_runner.sh

--- stage_migration_runner.sh
+++ stage_migration_runner.sh
@@ -5,6 +5,7 @@
 
 chmod +x rebuild_db.py
 
+echo "Running rebuild_db.py..."
 python3 rebuild_db.py $STG_ADMIN_DB_CONN_STRING $STG_TARGET_DB_CONN_STRING 'prod.dump' $STG_TARGET_DB
 
 ./create_db_user.sh $STG_TARGET_DB_CONN_STRING $STG_DB_USER $STG_DB_PASSWORD $STG_TARGET_DB
