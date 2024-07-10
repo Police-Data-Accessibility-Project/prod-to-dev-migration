8a1e88b1030a2e4ec1b9a56ab1334d9e24aeedab
maxachis <maxachis@gmail.com>
2024-07-10 11:10:33 -0400

    Updated sandbox_migration_runner.sh and stage_migration_runner.sh to use UserCreator.py for creating database users with developer privileges. Updated Jenkinsfile Dockerfile args to include SANDBOX_DEV_USER and SANDBOX_DEV_PASSWORD.

--- sandbox_migration_runner.sh
+++ sandbox_migration_runner.sh
@@ -5,6 +5,7 @@
 
 python3 rebuild_db.py $SANDBOX_ADMIN_DB_CONN_STRING $SANDBOX_TARGET_DB_CONN_STRING 'prod_to_sandbox.sql' $SANDBOX_TARGET_DB
 
-./create_db_user.sh $SANDBOX_TARGET_DB_CONN_STRING $SANDBOX_DB_USER $SANDBOX_DB_PASSWORD $SANDBOX_TARGET_DB
+python3 UserCreator.py --admin_db_conn_string $SANDBOX_TARGET_DB_CONN_STRING --dev_db_user $SANDBOX_DB_USER --dev_db_password $SANDBOX_DB_PASSWORD --target_db $SANDBOX_TARGET_DB
+python3 UserCreator.py --admin_db_conn_string $SANDBOX_TARGET_DB_CONN_STRING --dev_db_user $SANDBOX_DEV_USER --dev_db_password $SANDBOX_DEV_PASSWORD --target_db $SANDBOX_TARGET_DB --developer_privileges
 
 python3 load_csv_data.py $SANDBOX_TARGET_DB_CONN_STRING
--- stage_migration_runner.sh
+++ stage_migration_runner.sh
@@ -3,6 +3,7 @@
 # Dump and rebuild the stage database from production
 ./dump_prod.sh $PROD_DB_CONN_STRING
 
+
 python3 rebuild_db.py $STG_ADMIN_DB_CONN_STRING $STG_TARGET_DB_CONN_STRING 'prod.dump' $STG_TARGET_DB
 
-./create_db_user.sh $STG_TARGET_DB_CONN_STRING $STG_DB_USER $STG_DB_PASSWORD $STG_TARGET_DB
+python3 UserCreator.py --admin_db_conn_string $STG_TARGET_DB_CONN_STRING --dev_db_user $STG_DB_USER --dev_db_password $STG_DB_PASSWORD --target_db $STG_TARGET_DB
