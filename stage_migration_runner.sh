871d817b2314f71d0f250302149a099281747893
maxachis <maxachis@gmail.com>
2024-07-10 11:17:55 -0400

    Add descriptive echos of user creation actions

--- stage_migration_runner.sh
+++ stage_migration_runner.sh
@@ -6,4 +6,5 @@
 
 python3 rebuild_db.py $STG_ADMIN_DB_CONN_STRING $STG_TARGET_DB_CONN_STRING 'prod.dump' $STG_TARGET_DB
 
+echo "Creating app user"
 python3 UserCreator.py --admin_db_conn_string $STG_TARGET_DB_CONN_STRING --dev_db_user $STG_DB_USER --dev_db_password $STG_DB_PASSWORD --target_db $STG_TARGET_DB
