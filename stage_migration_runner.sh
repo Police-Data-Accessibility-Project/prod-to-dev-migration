8734ea647608f676de26db2a6367b80c959d9413
maxachis <maxachis@gmail.com>
2024-08-19 11:07:45 -0400

    Create relations_access_permission logic.

--- stage_migration_runner.sh
+++ stage_migration_runner.sh
@@ -10,3 +10,5 @@ python3 rebuild_db.py $STG_ADMIN_DB_CONN_STRING $STG_TARGET_DB_CONN_STRING 'prod
 
 echo "Creating app user"
 python3 UserCreator.py --admin_db_conn_string $STG_TARGET_DB_CONN_STRING --dev_db_user $STG_DB_USER --dev_db_password $STG_DB_PASSWORD --target_db $STG_TARGET_DB
+
+python3 relation_access_permissions/upload_relation_configurations_to_db.py ----connection_string $STG_TARGET_DB
