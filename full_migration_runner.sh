a5dd1055a643ce32b8c221896a7a3190d6288f8e
maxachis <maxachis@gmail.com>
2024-11-19 13:35:31 -0500

    Create AppUserCreator logic and add to `full_migration_runner.sh`

--- full_migration_runner.sh
+++ full_migration_runner.sh
@@ -7,7 +7,10 @@ set -e
 
 python3 rebuild_db.py $TARGET_ADMIN_DB_CONN_STRING $TARGET_DB_CONN_STRING 'prod.dump' $TARGET_DB
 
-echo "Creating app user"
+echo "Creating app database user"
 python3 UserCreator.py --admin_db_conn_string $TARGET_DB_CONN_STRING --dev_db_user $TARGET_DB_USER --dev_db_password $TARGET_DB_PASSWORD --target_db $TARGET_DB
+
+echo "Creating notifications app user"
+python3 AppUserCreator.py --admin_db_conn_string $TARGET_DB_CONN_STRING --user_email $NOTIFICATIONS_APP_USER_EMAIL --user_password $NOTIFICATIONS_APP_USER_PASSWORD --permission notifications
 
 python3 relation_access_permissions/upload_relation_configurations_to_db.py --connection_string $TARGET_DB_CONN_STRING
