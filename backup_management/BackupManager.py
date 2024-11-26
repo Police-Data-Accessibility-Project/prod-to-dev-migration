b3f9877638e2b426a654d16c6cfb6f892b64cbd8
maxachis <maxachis@gmail.com>
2024-11-26 15:07:12 -0500

    Start draft

---
+++ backup_management/BackupManager.py
@@ -0,0 +1,20 @@
+
+class BackupManager:
+
+    def __init__(self):
+        pass
+
+    def execute(self):
+
+        #
+        pass
+
+    def monthly_backup(self):
+
+        #
+
+
+        pass
+
+    def weekly_backup(self):
+        pass
