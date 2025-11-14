a8c5778ec4cba64b3eb7f3ee4843f550256df9e3
maxachis <maxachis@gmail.com>
2025-11-14 13:32:47 -0500

    Add `export PGPASSWORD`

--- source_manager/runner.sh
+++ source_manager/runner.sh
@@ -2,9 +2,10 @@
 
 set -e
 
+export PGPASSWORD=$PG_B_PASS
 # Drop and recreate database B
-psql -h $PG_B_HOST -U $PG_B_USER -u $PG_B_PASS -p $PG_B_PORT -c "DROP DATABASE IF EXISTS ${PG_B_DB};"
-psql -h $PG_B_HOST -U $PG_B_USER -u $PG_B_PASS -p $PG_B_PORT -c "CREATE DATABASE ${PG_B_DB};"
+psql -h $PG_B_HOST -U $PG_B_USER -p $PG_B_PORT -c "DROP DATABASE IF EXISTS ${PG_B_DB};"
+psql -h $PG_B_HOST -U $PG_B_USER -p $PG_B_PORT -c "CREATE DATABASE ${PG_B_DB};"
 
 # Run dump
 pg_dump \
