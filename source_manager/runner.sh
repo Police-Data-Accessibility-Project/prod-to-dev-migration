7165fb201d975a9029366fd21a222c6e6c3070a8
maxachis <maxachis@gmail.com>
2025-11-14 14:22:02 -0500

    Add no owner/privileges flags

--- source_manager/runner.sh
+++ source_manager/runner.sh
@@ -13,4 +13,6 @@ pg_dump \
   --dbname="postgresql://${PG_A_USER}:${PG_A_PASS}@${PG_A_HOST}:${PG_A_PORT}/${PG_A_DB}" \
   --format=custom \
 | pg_restore \
-    --dbname="postgresql://${PG_B_USER}:${PG_B_PASS}@${PG_B_HOST}:${PG_B_PORT}/${PG_B_DB}"
+    --dbname="postgresql://${PG_B_USER}:${PG_B_PASS}@${PG_B_HOST}:${PG_B_PORT}/${PG_B_DB}" \
+    --no-owner \
+    --no-privileges
