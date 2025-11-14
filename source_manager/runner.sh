eeb980c68a72a11d0a0fc41c21c86fdd6e8437ac
maxachis <maxachis@gmail.com>
2025-11-14 13:15:57 -0500

    Add port environment variable

--- source_manager/runner.sh
+++ source_manager/runner.sh
@@ -8,7 +8,7 @@ psql -h $PG_B_HOST -U $PG_B_USER -c "CREATE DATABASE ${PG_B_DB};"
 
 # Run dump
 pg_dump \
-  --dbname="postgresql://${PG_A_USER}:${PG_A_PASS}@${PG_A_HOST}:5432/${PG_A_DB}" \
+  --dbname="postgresql://${PG_A_USER}:${PG_A_PASS}@${PG_A_HOST}:${PG_A_PORT}/${PG_A_DB}" \
   --format=custom \
 | pg_restore \
-    --dbname="postgresql://${PG_B_USER}:${PG_B_PASS}@${PG_B_HOST}:5432/${PG_B_DB}"
+    --dbname="postgresql://${PG_B_USER}:${PG_B_PASS}@${PG_B_HOST}:${PG_B_PORT}/${PG_B_DB}"
